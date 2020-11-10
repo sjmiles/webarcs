d10eb38a979aedb6c7565d056f4fc3db6ffac3aa
Scott J. Miles <sjmiles@google.com>
2020-11-10 13:02:05 -0800

    tweaks

--- webarcs.5/arcs/ts/platforms/realms/realms.ts
+++ webarcs.5/arcs/ts/platforms/realms/realms.ts
@@ -8,7 +8,7 @@
  * http://polymer.github.io/PATENTS.txt
  */
 
-import Realm from '../../../../../realms-shim.esm.js'
+import Realm from '../../../../realms-shim.esm.js'
 
 const root = './particles';
 
